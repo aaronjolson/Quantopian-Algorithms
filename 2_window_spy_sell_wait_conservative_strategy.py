2ee8d13549dda6dadccacc85da8e2eb103252c31
Aaron J. Olson <aaolson817@gmail.com>
2015-12-13 18:54:14 -0600

    Check it out!

--- 2_window_spy_sell_wait_conservative_strategy.py
+++ 2_window_spy_sell_wait_conservative_strategy.py
@@ -1,18 +1,26 @@
-# Put any initialization logic here.  The context object will be passed to
-# the other methods in your algorithm.
 def initialize(context):
-    context.stocks = symbols('SPY')
+    context.spy = sid(8554)
 
 # Will be called on every trade event for the securities you specify.
 def handle_data(context, data):
-    for stock in context.stocks:
-        ma1 = data[stock].mavg(43)
-        ma2 = data[stock].mavg(180)
+    ma1 = data[context.spy].mavg(55)
+    ma2 = data[context.spy].mavg(200)
 
-        # price = data[stock].price
+    if ma1 > ma2:
+        order_target_percent(context.spy, 1.0)
+    elif ma1 < ma2:
+        order_target_percent(context.spy, 0.00)
 
-        if ma1 > ma2:
-            order_target_percent(stock, 0.99)
-
-        elif ma1 < ma2:
-            order_target_percent(stock, 0.00)
+"""
+some other numbers to try
+33, 205
+55, 205
+55, 195
+60, 450
+75, 425 //This is the highest result I have been able to find
+75, 430
+75, 440
+75, 450
+75, 455
+75, 465
+"""
