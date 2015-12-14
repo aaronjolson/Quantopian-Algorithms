10328f5084e862c3c732a4ac9ec9d7a97068b109
Aaron J. Olson <aaolson817@gmail.com>
2015-12-13 21:09:57 -0600

    Check it out!

---
+++ 2_window_sell_wait_for_minutes.py
@@ -0,0 +1,31 @@
+def initialize(context):
+    context.spy = sid(8554)
+
+    schedule_function(rebalance,
+                      date_rule = date_rules.every_day(),
+                      time_rule = time_rules.market_open())
+def rebalance(context, data):
+    ma1 = data[context.spy].mavg(55)
+    ma2 = data[context.spy].mavg(200)
+
+    if ma1 > ma2:
+        order_target_percent(context.spy, 1.0)
+    elif ma1 < ma2:
+        order_target_percent(context.spy, 0.00)
+
+# Will be called on every trade event for the securities you specify.
+def handle_data(context, data):
+    pass
+"""
+some other numbers to try
+33, 205
+55, 205
+55, 195
+60, 450
+75, 425 //This is the highest result I have been able to find
+75, 430
+75, 440
+75, 450
+75, 455
+75, 465
+"""
