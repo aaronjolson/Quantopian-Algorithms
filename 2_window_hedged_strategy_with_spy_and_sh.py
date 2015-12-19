7542a75a563ff87819050743f3806497c432e172
Aaron J. Olson <aaolson817@gmail.com>
2015-12-19 15:00:38 -0600

    Check it out!

--- 2_window_hedged_strategy_with_spy_and_sh.py
+++ 2_window_hedged_strategy_with_spy_and_sh.py
@@ -3,12 +3,12 @@ def initialize(context):
     context.sh = sid(32268)
 
 def handle_data(context, data):
-    ma1 = data[context.spy].mavg(33)
-    ma2 = data[context.spy].mavg(450)
+    mavg1 = data[context.spy].mavg(33)
+    mavg2 = data[context.spy].mavg(450)
 
-    if ma1 > ma2:
+    if mavg1 > mavg2:
         order_target_percent(context.sh, 0.00)
         order_target_percent(context.spy, 1.00)
-    elif ma1 < ma2:
+    elif mavg1 < mavg2:
         order_target_percent(context.spy, 0.00)
         order_target_percent(context.sh, 1.00)
